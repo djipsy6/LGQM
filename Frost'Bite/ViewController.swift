8434494dbf38849573ea603fe527fc9927beddc7
Djibril COLY <djipson5@me.com>
2020-04-17 21:12:10 +0000

    Initial Commit

---
+++ Frost'Bite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Frost'Bite
+//
+//  Created by Djibril Coly on 4/17/20.
+//  Copyright © 2020 NewAfricaTechnology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Frost'BiteTests/Frost_BiteTests.swift
@@ -0,0 +1,34 @@
+//
+//  Frost_BiteTests.swift
+//  Frost'BiteTests
+//
+//  Created by Djibril Coly on 4/17/20.
+//  Copyright © 2020 NewAfricaTechnology. All rights reserved.
+//
+
+import XCTest
+@testable import Frost_Bite
+
+class Frost_BiteTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
